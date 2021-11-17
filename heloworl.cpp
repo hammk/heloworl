2b6696ab64c3333a02fe32364d05f99757921133
Hamm Khan <dathambro4life@gmail.com>
2021-11-17 17:51:09 -0600

    made comments better

--- heloworl.cpp
+++ heloworl.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    std::cout << "helo; worl!!!\n"; // this wil say "helo; worl!!!"" also /n means linebreak so that a new line appears for every one of these lines
-    std::cout << "helo to you 2!\n"; // this wil reposnd to mydself!!
-    std::cout << "GameMaker Language is terrible!\n"; // GameMaker Language is terrible!
-    std::cout << "make in c++\n"; // this wil say "made in c++"
+    std::cout << "helo; worl!!!\n";                          // this wil say "helo; worl!!!"" also /n means linebreak so that a new line appears for every one of these lines
+    std::cout << "helo to you 2!\n";                         // this wil reposnd to mydself!!
+    std::cout << "GameMaker Language is terrible!\n";        // GameMaker Language is terrible!
+    std::cout << "make in c++\n";                            // this wil say "made in c++"
     return 0;
 }
