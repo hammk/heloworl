da41aff76eda6c786dd7a735f962300a93934c74
Hamm Khan <dathambro4life@gmail.com>
2021-11-17 18:06:17 -0600

    moar!!!!

--- theStanleyVarable.cpp
+++ theStanleyVarable.cpp
@@ -2,14 +2,24 @@
 
 int main()
 {
-    int x;                 // new variable called x
+    int x;                               // new variable called x
+    
     x = 36;
 
-    std::cout << x + "\n"; // x before being changed
+    std::cout << x + " changed to ";     // x before being changed
 
     x = 999;
 
-    std::cout << x + "\n"; // x after being changed
+    std::cout << x;                      // x after being changed
+
+    int gameing = 10;                    // gaeming 😳😳😳😳
+
+    int notGameing( 5 );                 // not gaemimng 😎😎😎😎😎😎😎😎😎😎😎😎
+
+    int socialCreditScore {-999999999};  // when more than one child in your repository 😱😱😱😱😱😱
+
+    int peopleWhoKnowWhatHappenedInTiennamenSquare {};
+    
 
     return 0;
 
